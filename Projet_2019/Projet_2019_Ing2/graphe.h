09c14390bac83fbcdaeb899878639a079bccb41a
VengeurClem <46677370+VengeurClem@users.noreply.github.com>
2019-04-17 12:31:59 +0200

    Pareto: Connexité
    PAS OPTIMISE
    à optimiser

--- Projet_2019/Projet_2019_Ing2/graphe.h
+++ Projet_2019/Projet_2019_Ing2/graphe.h
@@ -8,13 +8,17 @@ class graphe
  {
  public:
     graphe(std::string, std::string);
+    graphe(std::vector<float>, std::vector <arete>, std::vector <sommet>);
     void afficher() const;
     ~graphe();
-    float getPoids() const {return m_p;};
+    std::vector<float> getPoids() const {return m_p;};
+    std::vector<graphe> SolAd();
+    std::vector<graphe> Solutions();
+    std::vector <sommet> getSommets() const {return m_sommets;};
  protected:
 
  private:
-    float m_p;
+    std::vector<float> m_p;
     std::vector <arete> m_aretes;
     std::vector <sommet> m_sommets;
 };
--- Projet_2019/Projet_2019_Ing2/main.cpp
+++ Projet_2019/Projet_2019_Ing2/main.cpp
@@ -10,8 +10,17 @@
 int main()
 {
     std::cout << "Hello world!" << std::endl;
-    //graphe g{"../files/broadway.txt" , "../files/broadway_weights_0.txt"};
-    graphe g{"../files/triville.txt" , "../files/triville_weights_0.txt"};
-    g.afficher();
+    graphe g1{"../files/broadway.txt" , "../files/broadway_weights_0.txt"};
+    graphe g2{"../files/manhattan.txt" , "../files/manhattan_weights_0.txt"};
+    graphe g3{"../files/triville.txt" , "../files/triville_weights_0.txt"};
+    graphe g4{"../files/cubetown.txt" , "../files/cubetown_weights_0.txt"};
+    /*g1.afficher();
+    g2.afficher();
+    g3.afficher();
+    g4.afficher();*/
+    //g2.SolAd();
+    g1.SolAd();
+    //g3.SolAd();
+    //g4.SolAd();
     return 0;
 }
